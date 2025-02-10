84120a16f578d2635bbfae56f5f55f092f418b8f
Crystal Popeney <crystalpopeney@gmail.com>
2025-02-09 20:09:25 -0800

    process_info in progress

--- scraper.py
+++ scraper.py
@@ -3,38 +3,15 @@ from urllib.parse import urlparse, urldefrag
 from bs4 import BeautifulSoup
 import PartA as A
 import PartB as B
-# import lxml
+
+#TODO: relative --> absolute urls
+#TODO: traps? no url twice
+#TODO: get words & freqeuncies
+#TODO: subdomains for ics.uci.edu
+#TODO: do we want pdfs, .doc?
+
+
 """
-*.ics.uci.edu/*
-User-agent: *
-Disallow: /people
-Disallow: /happening
-
-*.cs.uci.edu/*
-User-agent: *
-Disallow: /people
-Disallow: /happening
-
-*.informatics.uci.edu/*
-Disallow: /wp-admin/
-Allow: /wp-admin/admin-ajax.php
-Allow: /research/labs-centers/
-Allow: /research/areas-of-expertise/
-Allow: /research/example-research-projects/
-Allow: /research/phd-research/
-Allow: /research/past-dissertations/
-Allow: /research/masters-research/
-Allow: /research/undergraduate-research/
-Allow: /research/gifts-grants/
-Disallow: /research/
-
-*.stat.uci.edu/*
-User-agent: *
-Disallow: /wp-admin/
-Allow: /wp-admin/admin-ajax.php
-
-Sitemap: https://www.stat.uci.edu/wp-sitemap.xml
-
 
  Implement the scraper function in scraper.py. The scraper function receives a URL
 and corresponding Web response (for example, the first one will be 
@@ -56,12 +33,29 @@ ALLOWED_DOMAINS = [
     r'.*\.stat\.uci\.edu'
 ]
 
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    unique_count = 0
+    valid_links = [link for link in links if is_valid(link)]
+
+    return valid_links
+
+def update_subdomains(sub_dict, parsed):
+    # assuming each parsed that comes in is unique
+    domain = parsed.netloc.lower()
+    if isinstance(domain, bytes):
+        domain = domain.decode('utf-8')
+    if re.match(r'.*\.ics\.uci\.edu', domain):
+        if domain in sub_dict:
+            sub_dict[domain] += 1
+        else:
+            sub_dict[domain] = 1
 
 def extract_next_links(url, resp):
     result = []
+    # keys are the subdomains, values are number of unique pages
+    ics_subdomains = {}
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -71,11 +65,64 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    html_doc = resp.raw_response.content
-    soup = BeautifulSoup(html_doc, "lxml")
-    for link in soup.find_all('a'):
-        result.append(urldefrag(link.get('href'))[0])
+    if resp.status == 200 or resp.status >= 300:
+        if resp.raw_response:
+            html_doc = resp.raw_response.content
+            # making sure the doc has data/content
+            if len(html_doc) > 0:
+                soup = BeautifulSoup(html_doc, "lxml")
+                
+                for link in soup.find_all('a'):
+                    href = link.get('href')
+                    parsed = urlparse(href)
+                    hyper = urldefrag(href)[0]
+                    # make sure don't get the same link twice from one page
+                    if hyper not in result:
+                        result.append(hyper)
+                        update_subdomains(ics_subdomains, parsed)
+                        process_info(clean_text, hyper)
+        
+    with open('report.txt', 'w') as report:
+        for sub in ics_subdomains:
+            report.write(f"{sub}, {ics_subdomains[sub]}")
     return result
+
+def tokenize(file_name):
+    token_list = []
+    with open(file_name, 'r') as file:
+        # read line by line to save memory
+        for line in file:
+            content = line.lower()
+            # get only alphanumeric characters
+            token_list.extend(re.findall(r"[0-9a-zA-Z]+", content))
+    return token_list
+
+def computeWordFrequencies(token_list):
+    # empty dict
+    token_frequencies = {}
+    # if dict does not contain token, add it. else, increment value.
+    for token in token_list:
+        if token not in token_frequencies:
+            token_frequencies[token] = 1
+        else:
+            token_frequencies[token] += 1
+    return token_frequencies
+
+def process_info(url, resp):
+    url_dict = {}
+    if resp.raw_response:
+        html_doc = resp.raw_response.content
+        # making sure the doc has data/content
+        if len(html_doc) > 0:
+            soup = BeautifulSoup(html_doc, "lxml")
+            soup_text = soup.get_text()
+            clean_text = re.sub(r'\s+','', soup_text)
+
+
+            with open('words.txt', 'w') as words:
+                words.write(clean_text)
+    tokens = tokenize('words.txt')
+    word_frequencies = computeWordFrequencies(tokens)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -85,7 +132,8 @@ def is_valid(url):
         
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
-            print(f"{url} NOT VALID")
+            with open('report.txt', 'w') as report:
+                report.write(f"{url} NOT VALID")
             return False
         if any(re.match(pattern, parsed.netloc.lower()) for pattern in ALLOWED_DOMAINS) and not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -96,10 +144,12 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
-            print(f"{url} IS VALID")
+            with open('report.txt', 'w') as report:
+                report.write(f"{url} IS VALID")
             return True
         else:
-            print(f"{url} NOT VALID")
+            with open('report.txt', 'w') as report:
+                report.write(f"{url} NOT VALID")
             return False
 
     except TypeError:
